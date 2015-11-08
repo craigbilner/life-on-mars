699984dbf8b889b1b1bfdd1ffe6f47701a09cd65
Craig Bilner <craig.bilner@gmail.com>
2015-11-08 20:25:52 +0000

    Add parsing logic

--- components/app/app.js
+++ components/app/app.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { missionControl } from '../../lib/robots';
+import { createSurface } from '../../lib/mars';
+import { moveMap, rotationMap, compassRotationMap, rotationCompassMap } from '../../lib/robotConfig';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      robots: [],
+      result: [],
+    }
   }
 
   getCompStyle() {
@@ -49,7 +55,47 @@ export default class App extends Component {
   }
 
   handleClick() {
-    console.log(this.state);
+    const [x, y] = this.state.surfaceDimensions.split(' ');
+    const surface = createSurface({
+      x: parseInt(x, 10),
+      y: parseInt(y, 10)
+    });
+
+    const control = missionControl({
+      moveMap,
+      rotationMap,
+      compassRotationMap,
+      rotationCompassMap,
+      surface,
+    });
+
+    const mappedRobots = this.state.robots.reduce((robotList, line, indx) => {
+      const newList = robotList.slice(0);
+      const isPosition = indx % 2 == 0;
+
+      if (isPosition) {
+        const [origX, origY, origOrient] = line.split(' ');
+        newList.push({
+          id: robotList.length + 1,
+          origX: parseInt(origX, 10),
+          origY: parseInt(origY, 10),
+          origOrient,
+        });
+      } else if (robotList[robotList.length - 1]) {
+        robotList[robotList.length - 1].instructions = line;
+      }
+
+      return newList;
+    }, []);
+
+    const processedRobots = control(mappedRobots);
+    const result = Object.keys(processedRobots).map(key => {
+      return processedRobots[key];
+    });
+
+    this.setState({
+      result
+    });
   }
 
   render() {
@@ -57,6 +103,15 @@ export default class App extends Component {
       <div style={this.getCompStyle()}>
         <textarea style={this.getTextAreaStyle()} onChange={this.handleChange.bind(this)}></textarea>
         <button style={this.getButtonStyle()} onClick={this.handleClick.bind(this)}>RUN</button>
+        <ul>
+          {
+            this.state.result.map(({ curX, curY, curOrient }) => {
+              return (
+                <div>{`${curX} ${curY} ${curOrient}`}</div>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }

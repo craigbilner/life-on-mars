bba0cbe9abd8381a2f775682e922129b2952b2d9
Craig Bilner <craig.bilner@gmail.com>
2015-11-08 19:43:26 +0000

    Add simple front end inputs

--- components/app/app.js
+++ components/app/app.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  getCompStyle() {
+    return {
+      color: 'green'
+    };
+  }
+
+  getTextAreaStyle() {
+    return {
+      color: 'green',
+      border: '1px solid green',
+      resize: 'none',
+      backgroundColor: 'black',
+      width: '100%',
+      height: 300,
+      fontSize: '3rem',
+      outline: 'none',
+    }
+  }
+
+  getButtonStyle() {
+    return {
+      float: 'right',
+      fontSize: '2rem',
+      color: 'green',
+      backgroundColor: 'black',
+      border: '1px solid green',
+      marginRight: -5,
+      outline: 'none',
+      cursor: 'pointer',
+    }
+  }
+
+  handleChange(evt) {
+    const [surfaceDimensions, ...robots] = evt.currentTarget.value.split('\n');
+
+    this.setState({
+      surfaceDimensions,
+      robots
+    });
+  }
+
+  handleClick() {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div>test</div>
+      <div style={this.getCompStyle()}>
+        <textarea style={this.getTextAreaStyle()} onChange={this.handleChange.bind(this)}></textarea>
+        <button style={this.getButtonStyle()} onClick={this.handleClick.bind(this)}>RUN</button>
+      </div>
     );
   }
 };
